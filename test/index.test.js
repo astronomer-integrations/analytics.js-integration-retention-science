df5df4b301d1ec09c34b13deea77f94546eaa805
Alois Barreras <alois@vicino.io>
2016-09-30 00:02:25 -0400

    updated deps

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,5 @@
 var Analytics = require('@astronomerio/analytics.js-core').constructor;
-var integration = require('@segment/analytics.js-integration');
+var integration = require('@astronomerio/analytics.js-integration');
 var sandbox = require('@segment/clear-env');
 var tester = require('@segment/analytics.js-integration-tester');
 var RetentionScience = require('../lib');
